bfec07c4932bb187b541ee130a3d47661d41aebc
Benedict Steffens <benedictsmail@gmx.de>
2019-05-15 14:45:07 +0200

    latest version

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ var slider = document.getElementById('test-slider');
    step: 1,
    orientation: 'horizontal', // 'horizontal' or 'vertical'
    range: {
-     'min': 0,
-     'max': 100
+     'min': 40,
+     'max': 60
    },
+   tooltips: [true, true],
    format: wNumb({
      decimals: 0
    })
@@ -15,26 +16,56 @@ var slider = document.getElementById('test-slider');
 
   var slider = document.getElementById('tree-number-slider');
     noUiSlider.create(slider, {
-     start: [20, 80],
+     start: [250, 950],
      connect: true,
      step: 1,
      orientation: 'horizontal', // 'horizontal' or 'vertical'
      range: {
-       'min': 0,
-       'max': 100
+       'min': 250,
+       'max': 950
      },
+     tooltips: [true, true],
      format: wNumb({
        decimals: 0
      })
     });
+
+    var slider = document.getElementById('tree-number-slider-popup');
+      noUiSlider.create(slider, {
+       start: 950,
+       connect: true,
+       step: 1,
+       orientation: 'horizontal', // 'horizontal' or 'vertical'
+       range: {
+         'min': 500,
+         'max': 950
+       },
+       tooltips: true,
+       format: wNumb({
+         decimals: 0
+       })
+      });
 
 
     $(document).ready(function(){
     $('.modal').modal();
   });
 
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([89.5250, -30.4500], 10);
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+map.on('click', function(e){
+    var marker = new L.marker(e.latlng).addTo(map);
+});
+
+function addMarker(e){
+// Add marker to map at click location; add popup window
+var newMarker = new L.marker(e.latlng).addTo(map);
+}
+
+
+  $('.dropdown-trigger').dropdown();
